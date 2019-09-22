90137b3d75cbb8bb8d6c8034757fef1b15dcbf08
Toby <tdudleyoffice@gmail.com>
2019-09-22 16:26:12 -0500

    refactor. Remove input from vectorSum & Scale, add inputrulesnumber.

--- lina.py
+++ lina.py
@@ -1,8 +1,8 @@
 import time
 
 
-def inputRules(vector):
-    """Defines rules for user entered  vectors"""
+def inputRulesVector(vector):
+    """Defines rules for user entered vectors"""
     # Check user entered vector using comma
     if "," not in vector:
         return False
@@ -17,48 +17,46 @@ def inputRules(vector):
     return True
 
 
+def inputRulesNumber(number):
+    """Defines rules for user entered numbers e.g. integer or float"""
+    try:
+        float(number)
+        return True
+    except ValueError:
+        print("Please enter a float or int")
+
+
 def convertToFloat(vector):
     """takes a user input vector as a string in 'a, b' format and outputs a list of floats"""
     return [float(i) for i in vector.split(",")]
 
 
-def vectorSum():
-    """Takes input of two vectors and outputs the linear combination of those two vectors as a tuple"""
+def getInputVector(quantity):
+    """Gets input from user, quantity is a string for first, second third etc"""
     while True:
-        vectorA = input("Please provide first vector in format a,b: ")
-        if inputRules(vectorA):
-            vectorA = convertToFloat(vectorA)
-            break
-    while True:
-        vectorB = input("please provide second vector in format c,d: ")
-        if inputRules(vectorB):
-            vectorB = convertToFloat(vectorB)
-            break
+        vectorA = input(f"Please provide {quantity} vector in format x,y: ")
+        if inputRulesVector(vectorA):
+            return convertToFloat(vectorA)
 
+
+def getInputNumber():
+    """Checks a value entered is float or integer before returning a float"""
+    while True:
+        scalar = input("Please enter a number by which to scale the vector: ")
+        if inputRulesNumber(scalar):
+            return float(scalar)
+
+
+def vectorSum(vectorA, vectorB):
+    """Takes input of two vectors and outputs the linear combination of those two vectors as a tuple"""
     # Do the sum, return it as a tuple
     vectorSum = (vectorA[0] + vectorB[0], vectorA[1] + vectorB[1])
     print(f"{vectorA} + {vectorB} = {vectorSum}")
     return vectorSum
 
 
-def vectorScale():
+def vectorScale(vector, scalar):
     """Scale a vector by a float"""
-    while True:
-        vector = input("Please enter a vector in the format a,b: ")
-        # Check vector entered is valid
-        if inputRules(vector):
-            vector = convertToFloat(vector)
-            break
-    while True:
-        scalar = input("Please enter a number by which to scale the vector: ")
-        # check vector entered is a number
-        try:
-            float(scalar)
-            scalar = float(scalar)
-            break
-        except ValueError:
-            print("Please enter a float or int")
-
     scaledVector = (vector[0] * scalar, vector[1] * scalar)
     print(f"{scalar} x {vector} = {scaledVector}")
     return scaledVector
@@ -72,10 +70,14 @@ def main():
             print("Thank you for using the linear algebra calculator")
             break
         if choice == "1":
-            vectorSum()
+            vectorA = getInputVector("first")
+            vectorB = getInputVector("second")
+            vectorSum(vectorA, vectorB)
             time.sleep(1)
         elif choice == "2":
-            vectorScale()
+            vector = getInputVector("a")
+            scalar = getInputNumber()
+            vectorScale(vector, scalar)
             time.sleep(1)
         else:
             print("invalid choice, enter a integer corresponding to the linear algebra calculation you would like to do")
--- test.py
+++ test.py
@@ -1,35 +1,34 @@
 import unittest
+from lina import inputRulesVector, convertToFloat, vectorSum
 
-from lina import inputRules, convertToFloat
 
-
-class TestInputRules(unittest.TestCase):
-    """Tests inputRules method with different valid and invalid inputs"""
+class TestinputRulesVectorVector(unittest.TestCase):
+    """Tests inputRulesVector method with different valid and invalid inputs"""
 
     def test_correct_integer_input(self):
-        self.assertEqual(inputRules("3,4"), True, "Should be True")
+        self.assertEqual(inputRulesVector("3,4"), True, "Should be True")
 
     def test_correct_float_input(self):
-        self.assertEqual(inputRules("3.2,4.2"), True, "Should be True")
+        self.assertEqual(inputRulesVector("3.2,4.2"), True, "Should be True")
 
     def test_spaces_after_comma(self):
-        self.assertEqual(inputRules("3,     9"), True, "Should be True")
+        self.assertEqual(inputRulesVector("3,     9"), True, "Should be True")
 
     def test_alphabetic_input(self):
-        self.assertEqual(inputRules("sdsdg, sdgdg"), False, "Should be False")
+        self.assertEqual(inputRulesVector("sdsdg, sdgdg"), False, "Should be False")
 
     def test_alphanumeric_input(self):
-        self.assertEqual(inputRules("3fr34, 43fer4f"), False, "Should be False")
+        self.assertEqual(inputRulesVector("3fr34, 43fer4f"), False, "Should be False")
 
     def test_no_comma(self):
-        self.assertEqual(inputRules("3034050948509345  390850943"), False, "Should be False")
+        self.assertEqual(inputRulesVector("3034050948509345  390850943"), False, "Should be False")
 
     def test_too_many_commas(self):
-        self.assertEqual(inputRules("3, 4, 5"), False, "Should be False")
+        self.assertEqual(inputRulesVector("3, 4, 5"), False, "Should be False")
 
 
 class TestConvertToFloat(unittest.TestCase):
-    """Function must have passed inputRules as true, tests it outputs correctly as float"""
+    """Function must have passed inputRulesVector as true, tests it outputs correctly as float"""
 
     def test_converts_int_string_to_float(self):
         self.assertEqual(convertToFloat("3,4"), [3.0, 4.0])
@@ -37,6 +36,11 @@ class TestConvertToFloat(unittest.TestCase):
     def test_converts_float_string_to_float(self):
         self.assertEqual(convertToFloat("3.345,9.235"), [3.345, 9.235])
 
+# Test input rules number
+
+# Test vector sum
+
+# Test vector scale
 
 if __name__ == '__main__':
     unittest.main()
